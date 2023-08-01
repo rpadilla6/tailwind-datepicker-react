c566def8c6ff3520fe34ccf1845c779bc70c5c6c
Ryan Padilla <ryan.padilla@vincit.fi>
2023-08-01 16:49:48 -0700

    Add missing style updates

---
+++ dist/config/styles.d.ts
@@ -0,0 +1,10 @@
+export declare const menuButtons: {
+    default: string;
+    selected: string;
+    unselected: string;
+};
+export declare const shared: {
+    disabled: string;
+    text: string;
+};
+//# sourceMappingURL=styles.d.ts.map
